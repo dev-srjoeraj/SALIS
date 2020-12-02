fd10da3850bd887e9165074207e9c7f927c5a6f4
srjoeraj <64455821+dev-srjoeraj@users.noreply.github.com>
2020-12-02 11:09:11 +0530

    Add files via upload

--- temp.py
+++ temp.py
@@ -4,6 +4,7 @@ from os import system as xec
 
 
 
-xec("pacstrap /mnt base linux linux-firmware base-devel vim nano")
+xec("pacstrap /mnt base linux linux-firmware base-devel vim nano figlet lolcat git")
 xec("genfstab -U /mnt >> /mnt/etc/fstab")
 xec("arch-chroot /mnt")
+xec("git clone https://github.com/dev-srjoeraj/SALIS")
